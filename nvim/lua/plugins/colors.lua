f0c53260f82be98de095658141e7a1ddd36e1a7a
JP Wienekus <jpwienekus@gmail.com>
2025-09-01 19:29:34 +0200

    update theme

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,24 +1,24 @@
-return { 
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000,
-  config = function ()
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end
-}
--- return {
---   'folke/tokyonight.nvim',
---   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     vim.cmd.colorscheme 'tokyonight-storm'
---     vim.cmd.hi 'Comment gui=none'
---
---     vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#FFFFFF', bold = true })
---     vim.api.nvim_set_hl(0, 'LineNr', { fg = '#FFFFFF', bold = true })
---     vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FFFFFF', bold = true })
---
---     vim.api.nvim_set_hl(0, 'StatusLine', { fg = '#FFFFFF', bold = true })
---     vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = '#FFFFFF', bold = false })
---   end,
+-- return { 
+--   "catppuccin/nvim", 
+--   name = "catppuccin", 
+--   priority = 1000,
+--   config = function ()
+--     vim.cmd.colorscheme "catppuccin-mocha"
+--   end
 -- }
+return {
+  'folke/tokyonight.nvim',
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    vim.cmd.colorscheme 'tokyonight-storm'
+    vim.cmd.hi 'Comment gui=none'
+
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#FFFFFF', bold = true })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg = '#FFFFFF', bold = true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FFFFFF', bold = true })
+
+    vim.api.nvim_set_hl(0, 'StatusLine', { fg = '#FFFFFF', bold = true })
+    vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = '#FFFFFF', bold = false })
+  end,
+}
