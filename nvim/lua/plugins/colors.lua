229e86022bde08284f06e105d5135cd51c551b2f
JP Wienekus <jpwienekus@gmail.com>
2025-09-01 19:34:11 +0200

    more theme fixes

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,6 +1,6 @@
--- return { 
---   "catppuccin/nvim", 
---   name = "catppuccin", 
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
 --   priority = 1000,
 --   config = function ()
 --     vim.cmd.colorscheme "catppuccin-mocha"
@@ -20,5 +20,39 @@ return {
 
     vim.api.nvim_set_hl(0, 'StatusLine', { fg = '#FFFFFF', bold = true })
     vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = '#FFFFFF', bold = false })
+
+    -- transparent background
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Terminal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "none" })
+
+    -- transparent notify background
+    vim.api.nvim_set_hl(0, "NotifyINFOBody", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyERRORBody", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyWARNBody", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyTRACEBody", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyDEBUGBody", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyINFOTitle", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyERRORTitle", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyWARNTitle", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyTRACETitle", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyDEBUGTitle", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyINFOBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyERRORBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyWARNBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyDEBUGBorder", { bg = "none" })
   end,
 }
